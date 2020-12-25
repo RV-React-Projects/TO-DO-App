b1a8a203f1a57aaeed595aa39398a1574e8f1dd4
Ranvijay Kumar <67736054+RanvijayChouhan12@users.noreply.github.com>
2020-12-25 17:46:47 +0530

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import './TodoList.css';
 import { Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { db } from './Firebase_Config';
